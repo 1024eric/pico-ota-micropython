422ee09b41486abf8bd8f39ab1d00d4a04cbe9e5
1024eric <105445337+1024eric@users.noreply.github.com>
2025-12-29 17:10:12 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 import ugit
 from machine import Pin
 import time
-#ugit.backup()
 ugit.pull_all()
-# NEW TEST
-#pin = Pin(0,Pin.IN,Pin.PULL_UP)
-#if pin.value() is 0:
-  #  ugit.pull_all()
-    
 #main code here
 TIME_MS=100
 LED = Pin("LED", Pin.OUT)
